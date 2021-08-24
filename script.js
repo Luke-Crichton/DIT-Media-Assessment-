37c30cd94f7d44e54b63d230fad67cd690810e2a
Luke-Crichton <18lcrichton@wakatipu.school.nz>
2021-08-24 12:25:06 +1200

    Added javascript. Is broken however

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
+
 function off() {
   document.getElementById("overlay").style.display = "none";
   document.getElementById("underlay").style.display = "block";
-  
+
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide(n) {
+  showSlides(slideIndex += n)
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementByClassName(mySlide)
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+}
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000);
 }
